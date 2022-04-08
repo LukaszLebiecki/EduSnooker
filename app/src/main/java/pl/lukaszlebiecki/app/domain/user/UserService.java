3f814bd56c7bdf69ee66b5b64f7854e9090432ec
LukaszLebiecki <lukasz.lebiecki@gmail.com>
2022-04-09 00:55:44 +0200

    EduSnooker-11 implement new webside

--- app/src/main/java/pl/lukaszlebiecki/app/domain/user/UserService.java
+++ app/src/main/java/pl/lukaszlebiecki/app/domain/user/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.lukaszlebiecki.app.domain.user.dto.UserCredentialsDto;
+import pl.lukaszlebiecki.app.domain.user.dto.UserInfoDto;
 import pl.lukaszlebiecki.app.domain.user.dto.UserRegistrationDto;
 
 import java.util.NoSuchElementException;
@@ -18,11 +19,13 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserRoleRepository userRoleRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserLevelRepository userLevelRepository;
 
-    public UserService(UserRepository userRepository, UserRoleRepository userRoleRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, UserRoleRepository userRoleRepository, PasswordEncoder passwordEncoder,UserLevelRepository userLevelRepository) {
         this.userRepository = userRepository;
         this.userRoleRepository = userRoleRepository;
         this.passwordEncoder = passwordEncoder;
+        this.userLevelRepository = userLevelRepository;
     }
 
     public Optional<UserCredentialsDto> findCredentialsByEmail(String email) {
@@ -30,11 +33,20 @@ public class UserService {
                 .map(UserCredentialsDtoMapper::map);
     }
 
+    public Optional<UserInfoDto> findUserByUserName(String userName) {
+        return userRepository.findByEmail(userName)
+                .map(UserInfoDtoMapper::map);
+    }
+
     @Transactional
     public void register(UserRegistrationDto registrationDto) {
         User user = new User();
         user.setName(registrationDto.getName());
         user.setEmail(registrationDto.getEmail());
+        UserLevel userLevel = userLevelRepository.save(new UserLevel());
+        userLevel.setLevel("l0.png");
+        userLevel.setProgress("00.png");
+        user.setUserLevel(userLevel);
         String passwordHash = passwordEncoder.encode(registrationDto.getPassword());
         user.setPassword(passwordHash);
         Optional<UserRole> userRole = userRoleRepository.findByName(USER_ROLE);
@@ -46,7 +58,6 @@ public class UserService {
         );
         userRepository.save(user);
     }
-
 
 
 }
--- app/src/main/java/pl/lukaszlebiecki/app/domain/web/HomeController.java
+++ app/src/main/java/pl/lukaszlebiecki/app/domain/web/HomeController.java
@@ -1,4 +1,37 @@
 package pl.lukaszlebiecki.app.domain.web;
 
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.lukaszlebiecki.app.domain.user.UserService;
+import pl.lukaszlebiecki.app.domain.user.dto.UserInfoDto;
+
+import java.util.Optional;
+
+@Controller
 class HomeController {
+
+    private final UserService userService;
+
+    HomeController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/")
+    String home(@CurrentSecurityContext SecurityContext securityContext, Model model) {
+        Optional<UserInfoDto> findUser = userService.findUserByUserName(securityContext.getAuthentication().getName());
+        if (findUser.isPresent()) {
+            UserInfoDto userInfoDto = findUser.get();
+            String name = userInfoDto.getName();
+            String level = userInfoDto.getLevel();
+            String progress = userInfoDto.getProgress();
+            model.addAttribute("name", name);
+            model.addAttribute("level", level);
+            model.addAttribute("progress", progress);
+        }
+        return "index";
+    }
 }
